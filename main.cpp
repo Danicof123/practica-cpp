7d729d3b940196358caa4cdf33dbcbc1ce2ec36a
Danicof123 <danicoof@gmail.com>
2025-01-11 18:54:38 -0300

    Agregar funcion suma(a,b)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
+template <typename T>
+T suma(T a, T b)
+{
+	return a + b;
+}
+
 int main()
 {
 	std::cout << "Hola mundo" << '\n';
-	
+	std::cout << "2 + 3 = " << suma(2,3) << '\n';
 	return 0;
 }
